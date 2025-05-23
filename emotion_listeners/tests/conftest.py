0a1edb29d39af67e8b94fa49a9bd08f5879023a5
Volker Goehler <volker.goehler@informatik.tu-freiberg.de>
2025-05-23 21:10:16 +0200

    fixed area calculation tests after refactoring

--- emotion_listeners/tests/conftest.py
+++ emotion_listeners/tests/conftest.py
@@ -2,14 +2,22 @@ import pytest
 from emotion_listeners.ledpanelemotioncontroller import LEDPanelEmotionController
 
 
+class CanvasStub(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
 @pytest.fixture
 def emotion_colors_fixture():
-    class CanvasStub(object):
-        def __init__(self, width, height):
-            self.width = width
-            self.height = height
 
     fixture = LEDPanelEmotionController()
     fixture.canvas = CanvasStub(1920, 1080)
     return fixture
 
+@pytest.fixture
+def ledpanel_controller_bare(monkeypatch):
+    # Override the __init__ to do nothing
+    monkeypatch.setattr(LEDPanelEmotionController, "__init__", lambda self: None)
+    instance = LEDPanelEmotionController()
+    instance.canvas = CanvasStub(1920, 1080)
+    return instance
--- emotion_listeners/tests/test_dimension_calc.py
+++ emotion_listeners/tests/test_dimension_calc.py
@@ -1,5 +1,5 @@
-def test_basic_case_with_px(emotion_colors_fixture):
-    result = emotion_colors_fixture.define_canvas("100px", "100px", "100px", "100px")
+def test_basic_case_with_px(ledpanel_controller_bare):
+    result = ledpanel_controller_bare.calculate_drawable_area("100px", "100px", "100px", "100px")
     assert result == {
         'top': {'x_start': 0, 'x_end': 1920, 'y_start': 0, 'y_end': 100},
         'bottom': {'x_start': 0, 'x_end': 1920, 'y_start': 980, 'y_end': 1080},
@@ -8,8 +8,8 @@ def test_basic_case_with_px(emotion_colors_fixture):
     }
 
 
-def test_mixed_units(emotion_colors_fixture):
-    result =  emotion_colors_fixture.define_canvas("10%", "15%", "100px", "20%")
+def test_mixed_units(ledpanel_controller_bare):
+    result =  ledpanel_controller_bare.calculate_drawable_area("10%", "15%", "100px", "20%")
     assert result == {
         'top': {'x_start': 0, 'x_end': 1920, 'y_start': 0, 'y_end': 108},
         'bottom': {'x_start': 0, 'x_end': 1920, 'y_start': 918, 'y_end': 1080},
@@ -18,8 +18,8 @@ def test_mixed_units(emotion_colors_fixture):
     }
 
 
-def test_full_percentage(emotion_colors_fixture):
-    result =  emotion_colors_fixture.define_canvas("50%", "50%", "25%", "25%")
+def test_full_percentage(ledpanel_controller_bare):
+    result =  ledpanel_controller_bare.calculate_drawable_area("50%", "50%", "25%", "25%")
     assert result == {
         'top': {'x_start': 0, 'x_end': 1920, 'y_start': 0, 'y_end': 540},
         'bottom': {'x_start': 0, 'x_end': 1920, 'y_start': 540, 'y_end': 1080},
@@ -28,16 +28,16 @@ def test_full_percentage(emotion_colors_fixture):
     }
 
 
-def test_exceeding_screen_size(emotion_colors_fixture):
+def test_exceeding_screen_size(ledpanel_controller_bare):
     try:
-        emotion_colors_fixture.define_canvas("600px", "600px", "600px", "600px")
+        ledpanel_controller_bare.calculate_drawable_area("600px", "600px", "600px", "600px")
     except ValueError as e:
         assert str(e) == "Invalid dimensions: excluded areas exceed the max dimensions."
 
 
 # Edge Cases
-def test_edge_case_no_exclusion(emotion_colors_fixture):
-    result = emotion_colors_fixture.define_canvas("0px", "0px", "0px", "0px")
+def test_edge_case_no_exclusion(ledpanel_controller_bare):
+    result = ledpanel_controller_bare.calculate_drawable_area("0px", "0px", "0px", "0px")
     assert result == {
         'top': {'x_start': 0, 'x_end': 1920, 'y_start': 0, 'y_end': 0},
         'bottom': {'x_start': 0, 'x_end': 1920, 'y_start': 1080, 'y_end': 1080},
@@ -46,8 +46,8 @@ def test_edge_case_no_exclusion(emotion_colors_fixture):
     }
 
 
-def test_edge_case_maximum_exclusion(emotion_colors_fixture):
-    result = emotion_colors_fixture.define_canvas("1080px", "0px", "1920px", "0px")
+def test_edge_case_maximum_exclusion(ledpanel_controller_bare):
+    result = ledpanel_controller_bare.calculate_drawable_area("1080px", "0px", "1920px", "0px")
     assert result == {
         'top': {'x_start': 0, 'x_end': 1920, 'y_start': 0, 'y_end': 1080},
         'bottom': {'x_start': 0, 'x_end': 1920, 'y_start': 1080, 'y_end': 1080},
